339134fa00e08f573e00c8eb73cfccd6a2fee6c7
panshi2016 <panshi2016@gmail.com>
2019-06-04 14:24:00 +0800

    commit baseline algorithms

--- Baseline_Algorithms/Local_Algorithms/Algorithms/PGDc_EMc/PGDc_EMc.m
+++ Baseline_Algorithms/Local_Algorithms/Algorithms/PGDc_EMc/PGDc_EMc.m
@@ -1,5 +1,10 @@
-function [] = PGDc_d() 
+function [] = PGDc_d(algoMode) 
 % PGDc-d: The projected gradient descent algorithm for optimizing sigma-conductance
+% EMc: The Expectation Maximization algorithm for optimizing sigma-conductance
+
+if nargin < 1
+    algoMode = 1;
+end
 
 graphPath = '../../../../test_data/zachary';
 communityPath = '../../../../test_data/zachary.txt';
@@ -20,7 +25,21 @@ seed = comm{commId}(seedId);
 seed_vec = zeros(length(graph),1);
 seed_vec(seed) = 1;
 
-[set,l] = optimize_cluster(graph,seed_vec,'score','sigma');
+if algoMode == 1
+    [set,l] = optimize_cluster(graph,seed_vec);
+    savePathandName = '../../Results/zachary/result_PGDc-0.txt';
+elseif algoMode == 2
+    [set,l] = optimize_cluster(graph,seed_vec,'score','sigma');
+    savePathandName = '../../Results/zachary/result_PGDc-d.txt';
+elseif algoMode == 3
+    [set,l] = optimize_cluster(graph,seed_vec,'method','em','sigma',0);
+    savePathandName = '../../Results/zachary/result_EMc-0.txt';
+elseif algoMode == 4
+    [set,l] = optimize_cluster(graph,seed_vec,'method','em');
+    savePathandName = '../../Results/zachary/result_EMc-d.txt';
+else
+    error('Not a valid algorithm mode');
+end
 set = find(set);
 
 % compute F1 score and Jaccard index
@@ -38,7 +57,6 @@ disp(set')
 fprintf('The F1 score and Jaccard index between detected community and ground truth community are %.3f and %.3f\n',F1,Jaccard)
 
 % save out result
-savePathandName = '../../Results/zachary/result_PGDc-d.txt';
 dlmwrite(savePathandName,'The detected community is','delimiter','');
 dlmwrite(savePathandName,set','-append','delimiter','\t','precision','%.0f');
 dlmwrite(savePathandName,['The F1 score and Jaccard index between detected community and ground truth community are ' num2str(F1,'%.3f') ' and ' num2str(Jaccard,'%.3f')],'-append','delimiter','');
